cc1a782b3caebf5234edaa3b66350857eafc0954
Kratos768 <nimcet2k24@gmail.com>
2025-05-02 11:30:42 +0530

    Update StarDownwardPattern
    Star Pattern in descending order

--- src/StarDownwardPattern.java
+++ src/StarDownwardPattern.java
@@ -3,6 +3,12 @@ public class StarDownwardPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Range No. of Row");
-        
+        int row=sc.nextInt();
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
